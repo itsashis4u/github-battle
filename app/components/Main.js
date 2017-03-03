88d1182576ecb64492564dc64785dbc9adb3db41
Ashish Kumar Badtiya <ashishkumar@keepworks.com>
2017-03-03 23:06:04 +0530

    Added transition between pages

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-const Main = props => (
-  <div className="main-container">
-    {props.children}
-  </div>
-);
+require('../main.css');
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main-container">
+        <ReactCSSTransitionGroup
+          transitionName="appear"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}
+        >
+          {React.cloneElement(this.props.children, { key: this.props.location.pathname })}
+        </ReactCSSTransitionGroup>
+      </div>
+    );
+  }
+}
 
 export default Main;
